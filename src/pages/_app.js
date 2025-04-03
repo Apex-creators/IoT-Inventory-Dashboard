4ff1401a5ad57ea54c00c2749ca8f3faa2fbb685
Apex-creators <jerinthomas5611@gmail.com>
2025-04-03 14:20:22 -0400

    _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,14 @@
+
 import '../styles/globals.css'
+import Header from '../components/Header'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className="min-h-screen bg-gray-50 text-gray-800">
+      <Header />
+      <main className="px-4 md:px-12">
+        <Component {...pageProps} />
+      </main>
+    </div>
+  )
 }
