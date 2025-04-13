3fd4a26b9cb24743d29805dc5fbe419a32fd9ea4
Apex-creators <jerinthomas5611@gmail.com>
2025-04-13 03:51:18 -0400

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,27 +1,16 @@
-// src/App.js
-import React from 'react';
-import NavigationBar from '../components/NavigationBar';
-import Home from '../components/Home';
-import InventoryReport from '../components/InventoryReport';
-import CompanyDashboard from '../components/CompanyDashboard';
-import DataAnalysisDashboard from '../components/DataAnalysisDashboard';
-import ProjectInfo from '../components/ProjectInfo';
+// src/pages/_app.js
+import '../styles/globals.css'; // Global CSS
+import NavigationBar from '../components/NavigationBar'; // Import the navigation bar
 
-function App() {
+function MyApp({ Component, pageProps }) {
   return (
-    <div>
+    <>
       <NavigationBar />
       <div className="container mt-4">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/inventory-report" element={<InventoryReport />} />
-          <Route path="/company-dashboard" element={<CompanyDashboard />} />
-          <Route path="/data-analysis" element={<DataAnalysisDashboard />} />
-          <Route path="/project-info" element={<ProjectInfo />} />
-        </Routes>
+        <Component {...pageProps} />
       </div>
-    </div>
+    </>
   );
 }
 
-export default App;
+export default MyApp;
