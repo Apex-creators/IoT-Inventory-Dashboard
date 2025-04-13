7d4999f29c78d7ee1d0a904788ca8b6b2005187b
Apex-creators <jerinthomas5611@gmail.com>
2025-04-13 16:36:38 -0400

    Update button.js

--- src/components/ui/button.js
+++ src/components/ui/button.js
@@ -1,11 +1,12 @@
 // src/components/ui/button.js
 import React from 'react';
 
-export default function Button({ children, onClick }) {
+export function Button({ children, onClick, className = "", ...props }) {
   return (
     <button
-      className="px-4 py-2 bg-blue-600 text-white font-semibold rounded hover:bg-blue-700"
       onClick={onClick}
+      className={`px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 ${className}`}
+      {...props}
     >
       {children}
     </button>
